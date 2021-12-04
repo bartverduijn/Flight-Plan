63929fe081ed853e5b44b11ad1f75cbf2bf2b755
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2021-12-04 23:03:45 +0100

    set up remix

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+		commonjs: true,
+	},
+	extends: [
+		'airbnb-typescript',
+		'airbnb/hooks',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:prettier/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: ['./tsconfig.json'],
+		sourceType: 'module',
+		ecmaVersion: 12,
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	plugins: ['react', 'import'],
+	rules: {
+		indent: ['error', 'tab'],
+		'no-tabs': 'off',
+		'no-unused-vars': [
+			'warn',
+			{
+				argsIgnorePattern: 'res|next|^err',
+			},
+		],
+		'react/jsx-filename-extension': 'off',
+		'react/jsx-indent': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-uses-react': 'off',
+	},
+};
