0a87076b0d5193a1c89b93c078ab4f84e0d0a4dc
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2021-12-05 10:40:46 +0100

    add testdata

---
+++ prisma/seed.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+const db = new PrismaClient();
+
+function getProjects() {
+	return [{ name: 'Work' }, { name: 'Home' }, { name: 'Someday_Maybe' }];
+}
+
+async function seed() {
+	await Promise.all(
+		getProjects().map(project => {
+			return db.project.create({ data: project });
+		})
+	);
+}
+
+seed().catch(err => console.error(err));
